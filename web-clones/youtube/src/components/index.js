244c7347f6fe437f0bf54eb2baf18d73854ae1b4
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-10-08 00:58:05 +0200

    sidebar created

--- web-clones/youtube/src/components/index.js
+++ web-clones/youtube/src/components/index.js
@@ -4,3 +4,4 @@ export { default as Navbar } from "./Navbar";
 export { default as SearchFeed } from "./SearchFeed";
 export { default as SearchBar } from "./Searchbar";
 export { default as VideoDetail } from "./VideoDetail";
+export { default as Sidebar } from "./Sidebar";

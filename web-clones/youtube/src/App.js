747c7858705a63d8ce442d9d96b93203593113cd
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-10-07 17:46:21 +0200

    routing added

--- web-clones/youtube/src/App.js
+++ web-clones/youtube/src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import { Box } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from "./components";
 
-const app = () => {
-  return <div>app</div>;
-};
+const app = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default app;
--- web-clones/youtube/src/index.js
+++ web-clones/youtube/src/index.js
@@ -1,6 +1,10 @@
+import "@fontsource/roboto/300.css";
 import React from "react";
-import { ReactDOM } from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 import App from "./app";
 
-const root = ReactDOM.createRoot(document.getElementByiD("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<App />);

7cc9de38166c60ff4dab573e83eb5399925a7eaa
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-10-11 22:32:59 +0200

    moved all files to main folder and fixed env API error

--- web-clones/youtube/src/components/index.js
+++
@@ -1,8 +0,0 @@
-export { default as ChannelDetail } from "./ChannelDetail";
-export { default as Feed } from "./Feed";
-export { default as Navbar } from "./Navbar";
-export { default as SearchFeed } from "./SearchFeed";
-export { default as SearchBar } from "./Searchbar";
-export { default as VideoDetail } from "./VideoDetail";
-export { default as Sidebar } from "./Sidebar";
-export { default as Videos } from "./Videos";

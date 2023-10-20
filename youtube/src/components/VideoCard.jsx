0e7938fe8b5565461f2fe797d23d7a06f343868f
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-10-21 01:54:36 +0200

    added loader and videocards

---
+++ youtube/src/components/VideoCard.jsx
@@ -0,0 +1,11 @@
+import { CheckCircle } from '@mui/icons-material';
+import { Card, CardContent, CardMedia, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import { demoThumbnailUrl } from '../utils/constants';
+
+const VideoCard = () => {
+  return <div>VideoCard</div>;
+};
+
+export default VideoCard;

cd3ac5154a2792f3cddccae097f15265ff7e2b2a
Hubert Zawisza <hubert.zawisza90@gmail.com>
2023-10-21 14:48:36 +0200

    videocard and loader added, thumbnails

--- youtube/src/components/VideoCard.jsx
+++ youtube/src/components/VideoCard.jsx
@@ -2,10 +2,31 @@ import { CheckCircle } from '@mui/icons-material';
 import { Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-import { demoThumbnailUrl } from '../utils/constants';
+import {
+  demoChannelTitle,
+  demoChannelUrl,
+  demoThumbnailUrl,
+  demoVideoTitle,
+  demoVideoUrl,
+} from '../utils/constants';
 
-const VideoCard = () => {
-  return <div>VideoCard</div>;
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+    </Card>
+  );
 };
 
 export default VideoCard;
